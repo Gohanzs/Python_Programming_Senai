a0516b0a9d4f5f455dab00afa981744c59f60b31
Gohanzs <120071779+Gohanzs@users.noreply.github.com>
2026-01-31 14:23:02 -0300

    Add files via upload

---
+++ Calculadora Básica.py
@@ -0,0 +1,15 @@
+print('Calculadora')
+print('------------------' *2)
+
+numero = float(input('Digite um numero:'))
+numero_2 = float (input('Digite um numero:'))
+
+soma = numero + numero_2
+subtracao = numero - numero_2
+mult = numero * numero_2
+div = numero / numero_2
+
+print('O Resultado somado é::',soma)
+print('O Resultado subtraido é:',subtracao)    
+print('O Resultado multiplicado é:',mult)
+print('O Resultado dividido é:',div)
