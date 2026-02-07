1dd94bc766495fb66db0272db74352ae9093bc76
Gohanzs <120071779+Gohanzs@users.noreply.github.com>
2026-02-07 12:01:33 -0300

    Add files via upload

---
+++ Aula 7/mercado.py
@@ -0,0 +1,25 @@
+#E-commerce
+# lista de podutos
+# lista de valores
+# poder comprar um produto
+print('E-COMMERCE')
+
+lista_prod = ['','hd', 'pen-drive', 'fone', 'carregador']
+lista_valores = [0,450.0,100.0,30.0,90,0]
+
+print(f'''
+ PRODUTOS:
+      ID   PRODUTO VALOR R$     
+        {lista_prod.index('hd')}                    {lista_prod[1]}     {lista_valores[1]}
+        {lista_prod.index('pen-drive')}             {lista_prod[2]}     {lista_valores[2]}
+        {lista_prod.index('fone')}                  {lista_prod[3]}     {lista_valores[3]}
+        {lista_prod.index('carregador')}            {lista_prod[4]}     {lista_valores[4]} 
+
+      ''')
+
+
+
+
+
+
+
